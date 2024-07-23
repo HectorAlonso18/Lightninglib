afaec3ff7e6a717790205627d2cea3fbf912a5f9
Hector <eltortkd@outlook.com>
2024-07-23 12:45:44 -0600

    Changes in doxygen comments

--- include/lightninglib/Trapezoidal.hpp
+++ include/lightninglib/Trapezoidal.hpp
@@ -29,8 +29,8 @@ private:
 public:
   /**
    *@brief Trapezoidal profile constructor. Create a new trapezoidal profile. 
-   *@param max_velocity what would be the maximum profile velocity?
-   *@param max_acceleration what would be the maximum profile accleration? 
+   *@param max_velocity what would be the maximum profile velocity in inches/second?
+   *@param max_acceleration what would be the maximum profile accleration in inches/second^2? 
    */
   TrapezoidalProfile(float max_velocity, float max_acceleration);
   ~TrapezoidalProfile() = default;
@@ -58,69 +58,69 @@ public:
   
   /**
     *@brief sets the main parameters for the profile. 
-    *@param max_velocity what would be the maximum profile velocity?
-    *@param max_acceleration what would be the maximum profile accleration? 
+    *@param max_velocity what would be the maximum profile velocity (inches/second)?
+    *@param max_acceleration what would be the maximum profile accleration (inches/second^2)? 
    */
   void set_parameters(const float max_velocity, const float max_acceleration); 
   
   /**
     *@brief sets the maximum profile velocity. 
-    *@param max_velocity what would be the maximum profile velocity?
+    *@param max_velocity what would be the maximum profile velocity(inches/second)?
    */
   void set_max_velocity(const float max_velocity);
 
   /**
     *@brief sets the maximum profile acceleration. 
-    *@param max_acceleration what would be the maximum profile acceleration?
+    *@param max_acceleration what would be the maximum profile acceleration (inches/second^2)?
   */
   void set_max_acceleration(const float max_acceleration);
 
   /**
     *@brief gets the desired velocity giving a point. 
     *@param index the profile index. 
-    *@return the desired velocity. 
+    *@return the desired velocity (inches/second). 
    */ 
   float get_velocity_at(int index) const;
 
   /**
     *@brief gets the maximum profile velocity. 
-    *@return the maximum velocity. 
+    *@return the maximum velocity (inches/second). 
    */
   float get_max_velocity() const;
 
   /**
     *@brief gets the maximum profile acceleration.
-    *@return the maximum acceleration.  
+    *@return the maximum acceleration (inches/second^2).  
    */
   float get_max_acceleration() const;
   
   /**
     *@brief gets the acceleration time.
-    *@return the acceleration time.   
+    *@return the acceleration time (seconds).   
    */
   float get_accel_time() const;
   
   /**
     *@brief gets the acceleration distance (acceleration phase).  
-    *@return the acceleration time. 
+    *@return the acceleration distance (inches). 
    */
   float get_accel_distance() const;
   
   /**
     *@brief gets the desacceleration distance (desacceleration phase). 
-    *@return gets the desacceleration time. 
+    *@return gets the desacceleration distance (inches). 
    */
   float get_desaccel_distance() const;
   
   /**
     *@brief gets the constant velocity distance (constant acceleration phase). 
-    *@return gets the constant velocity distance. 
+    *@return gets the constant velocity distance (inches). 
    */
   float get_velocity_constant_distance() const;
   
   /**
     *@brief gets the time to arrive. 
-    *@return the time to arrive. 
+    *@return the time to arrive (second). 
    */
   float get_time_to_arrived() const; 
   
