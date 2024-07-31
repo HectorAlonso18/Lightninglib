6297cb7871f4ad1ba768c4b0dfdaa668fa155d56
Hector <eltortkd@outlook.com>
2024-07-30 18:03:12 -0600

    updated urls

--- lightning-docs/docusaurus.config.js
+++ lightning-docs/docusaurus.config.js
@@ -16,15 +16,17 @@ const config = {
   favicon: 'img/lightning_logo.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://HectorAlonso18.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Lightninglib',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'HectorAlonso18', // Usually your GitHub org/user name.
+  projectName: 'Lightninglib', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
